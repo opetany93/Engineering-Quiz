5bcb99e45c854f0aba6c65600f47a406482523d8
opetany <bochynski.arkadiusz@gmail.com>
2016-03-15 10:24:19 +0100

    registrationActivity added

--- app/src/main/java/net/ddns/opetany/engineeringquiz/registrationActivity.java
+++ app/src/main/java/net/ddns/opetany/engineeringquiz/registrationActivity.java
@@ -1,7 +1,9 @@
 package net.ddns.opetany.engineeringquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void registrationDoneButton(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
     }
 }
