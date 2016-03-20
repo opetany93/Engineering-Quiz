102b3089545ed68278198d3d1883be41365bc853
opetany <bochynski.arkadiusz@gmail.com>
2016-03-20 17:59:22 +0100

    coraz blizej dzialajacego logowania

--- app/src/main/java/net/ddns/opetany/engineeringquiz/RegistrationActivity.java
+++ app/src/main/java/net/ddns/opetany/engineeringquiz/RegistrationActivity.java
@@ -1,8 +1,8 @@
 package net.ddns.opetany.engineeringquiz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
